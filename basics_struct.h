8f9018f6ffd932a639e523f733ce17d256320186
nathjone <nathaniel_jones@selinc.com>
2022-03-28 13:33:54 -0700

    Move struct basics code into basics_struct.h
    Next step: Directory structure with "src" and "include", .cpp
    and .h files.

---
+++ basics_struct.h
@@ -0,0 +1,36 @@
+#ifndef BASICS_STRUCT_H
+#define BASICS_STRUCT_H
+
+#include <iostream>
+
+using namespace std;
+
+struct Rectangle {
+    int16_t width;
+    int16_t height;
+    char x;
+};
+
+void print_rectangle() {
+    struct Rectangle r1 = {10, 5};
+
+    r1.width = 12;
+
+    cout << "r1.width: " << r1.width << "\n";
+    cout << "r1.height: " << r1.height << "\n";
+
+    // Becuase it's easier for the computer to read fields of the same size,
+    // sizeof() will show 2 + 2 + 2 = 6 bytes (not 5). The char x field takes
+    // 2 bytes instead of 1.
+    // If you change the type of width and height to int32_t, you'll see that
+    // the char x field will then take up 4 bytes.
+    //
+    // You can print the size of the class or the instance.
+    // Here, the instance is used.
+    //
+    // Note the "%lu" (long unsigned) format spec code, rather than "%d".
+    // Using "%d" will issue a compiler warning.
+    printf("%lu\n", sizeof(r1));
+}
+
+#endif // BASICS_STRUCT_H
