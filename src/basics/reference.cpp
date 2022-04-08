a01c0338ebe60a26d9bc754b595e15dc62c36d73
nathjone <nathaniel_jones@selinc.com>
2022-04-07 17:02:45 -0700

    Fill out reference.cpp

--- src/basics/reference.cpp
+++ src/basics/reference.cpp
@@ -3,5 +3,22 @@
 using namespace std;
 
 void print_reference() {
-    cout << "yo" << "\n";
+    int a = 10;
+    // NOTE: References require initialization at the same time as
+    // declaration.
+    // Analogy: a's nickname. E.g. Rajendran -> Raj.
+    int& r = a;
+
+    int b = 25;
+    r = b; // Doing this sets r and a to the same value as b.
+
+    printf("a: %i\n", a); // %i is the same as %d.
+    printf("b: %i\n", b);
+    printf("r: %i\n", r);
+
+    // CONCEPT: Reference doesn't consume any memory.
+    // Behold, the reference has the same address as the
+    // referenced variable. They are one.
+    printf("address of a: %p\n", &a);
+    printf("address of r: %p\n", &r);
 }
